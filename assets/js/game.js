22bdd6f265e51096a95420911313f727018b91a8
MesH <meshtatsuo@gmail.com>
2022-01-05 19:26:14 -0700

    shop feature functional. Added variables to track player spending

--- assets/js/game.js
+++ assets/js/game.js
@@ -11,15 +11,21 @@ var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
+//shop items and costs
+var upgradeAmount = 6;
+var healAmount = 20;
+var upgradeCost = 7;
+var healCost = 7;
+var skipCost = 2;
+
 //Functions
 
 var startGame = function () {
+  // reset player stats
+  playerHealth = 100;
+  playerAttack = 10;
+  playerMoney = 10;
   for (var i = 0; i < enemyNames.length; i++) {
-    // reset player stats
-    playerHealth = 100;
-    playerAttack = 10;
-    playerMoney = 10;
-
     if (playerHealth > 0) {
       // let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
       window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
@@ -35,6 +41,12 @@ var startGame = function () {
 
       // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
       fight(pickedEnemyName);
+
+      // if we're not at the last enemy in the array
+      if (i < enemyNames.length - 1) {
+        shop();
+      }
+
       if (playerHealth <= 0) {
         window.alert("You have lost your robot in battle! Game Over!");
         break;
@@ -70,17 +82,56 @@ var endGame = function () {
 
 //shopping
 var shop = function () {
+  console.log("Player has entered the shop");
+
+  if (playerMoney > 0) {
+    window.alert(
+      "Welcome to the shop! You currently have " + playerMoney + " coins."
+    );
+  } else {
+    playerMoney = 0;
+    window.alert("You have no money to shop with, sorry!");
+  }
   var purchase = window.prompt(
     "Would you like to buy a REFILL, an UPGRADE, or LEAVE?"
   );
 
   switch (purchase.toUpperCase()) {
     case "LEAVE":
+      window.alert("Thank you for stopping by! Good luck in the next round!");
       break;
     case "REFILL":
-      break;
-    case "ATTACK":
-      break;
+      if (playerMoney >= healCost) {
+        window.alert("Refilling player's health by 20 for 7 dollars.");
+
+        //increase health and decrease money
+        playerHealth += 20;
+        playerMoney -= 7;
+        break;
+      } else {
+        window.alert("You can't afford this item. Sorry!");
+        break;
+      }
+
+    case "UPGRADE":
+      if (playerMoney >= upgradeCost) {
+        window.alert("Upgrading player's attack by 6 for 7 dollars.");
+
+        //increase ttack and decrease money
+        playerAttack += upgradeAmount;
+        console.log(playerAttack);
+        playerMoney -= upgradeCost;
+        console.log(playerMoney);
+        break;
+      } else {
+        window.alert("You don't have the money. sorry!");
+        break;
+      }
+    default:
+      window.alert("You did not pick a valid option. Try again.");
+
+      // call shop() again to force player to pick a valid option
+      shop();
   }
 };
 
@@ -94,13 +145,16 @@ var fight = function (enemyName) {
     if (promptFight === "skip" || promptFight === "SKIP") {
       var confirmSkip = window.confirm("Are you sure?");
       //if yes, deduct money and quit
-      if (confirmSkip) {
-        playerMoney -= 2;
+      if (confirmSkip && playerMoney >= skipCost) {
+        playerMoney -= skipCost;
         window.alert(playerName + " has chosen to skip the fight. Goodbye!");
         break;
       }
       // if no, run fight() again.
-      else {
+      else if (playerMoney < skipCost) {
+        window.alert("You can't afford to skip!");
+        fight();
+      } else {
         fight();
       }
     }
