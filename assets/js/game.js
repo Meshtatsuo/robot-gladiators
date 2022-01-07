e7869a0ea3cf2741173f91ef8d99c4f7205a49d8
MesH <meshtatsuo@gmail.com>
2022-01-07 16:22:03 -0700

    added rng damage

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,3 +1,8 @@
+var randomNumber = function (min, max) {
+  var value = Math.floor(Math.random() * (max - min + 1) + min);
+  return value;
+};
+
 // Initialize player info
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
@@ -8,7 +13,7 @@ var playerMoney = 10;
 console.log(playerName, playerAttack, playerHealth);
 
 var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
-var enemyHealth = 50;
+var enemyHealth = randomNumber(40, 60);
 var enemyAttack = 12;
 
 //shop items and costs
@@ -34,7 +39,7 @@ var startGame = function () {
       var pickedEnemyName = enemyNames[i];
 
       // reset enemyHealth before starting new fight
-      enemyHealth = 50;
+      enemyHealth = randomNumber(40, 60);
 
       // use debugger to pause script from running and check what's going on at that moment in the code
       // debugger;
@@ -106,7 +111,7 @@ var shop = function () {
 
         //increase health and decrease money
         playerHealth += 20;
-        playerMoney -= 7;
+        playerMoney = playerMoney = Math.max(0, playerMoney - healCost);
         break;
       } else {
         window.alert("You can't afford this item. Sorry!");
@@ -118,10 +123,8 @@ var shop = function () {
         window.alert("Upgrading player's attack by 6 for 7 dollars.");
 
         //increase ttack and decrease money
-        playerAttack += upgradeAmount;
+        playerMoney = Math.max(0, playerMoney - upgradeCost);
         console.log(playerAttack);
-        playerMoney -= upgradeCost;
-        console.log(playerMoney);
         break;
       } else {
         window.alert("You don't have the money. sorry!");
@@ -160,9 +163,9 @@ var fight = function (enemyName) {
     }
     // if player chooses to fight, then fight
     else if (promptFight === "fight" || promptFight === "FIGHT") {
-      //subtract value of playerAttack from value of  enemyHealth
-      //and use that result to update the value in the `enemyHealth` variable
-      enemyHealth = enemyHealth - playerAttack;
+      // generate random damage value based on player attack power
+      var damage = randomNumber(playerAttack - 3, playerAttack);
+      enemyHealth = Math.max(0, enemyHealth - damage);
 
       // Log a resulting message to the console so we know that it worked.
       console.log(
@@ -171,9 +174,9 @@ var fight = function (enemyName) {
           enemyName +
           ". " +
           enemyName +
-          " now has " +
-          enemyHealth +
-          " health."
+          " for " +
+          damage +
+          "."
       );
 
       // check enemy's health
@@ -184,19 +187,13 @@ var fight = function (enemyName) {
         window.alert(enemyName + " still has " + enemyHealth + " health left.");
       }
 
-      // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
-      playerHealth = playerHealth - enemyAttack;
+      //deal random damage to player
+      var damage = randomNumber(enemyAttack - 3, enemyAttack);
+      playerHealth = Math.max(0, playerHealth - damage);
 
       // Log a resulting message to the console so we know that it worked.
       console.log(
-        enemyName +
-          " attacked " +
-          playerName +
-          ". " +
-          playerName +
-          " now has " +
-          playerHealth +
-          " health."
+        enemyName + " attacked " + playerName + " for " + damage + "."
       );
 
       // Check player's health
